0d17b605cdb3bc2cf4c13b40e8ec66111437c286
poonamk <poonamk@thoughtwin.com>
2023-02-17 18:29:56 +0530

    add test

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,9 +4,9 @@
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
-sudo cp /home/ubuntu/cicd/nginx/nginx.conf /etc/nginx/sites-available/cicd
-sudo ln -s /etc/nginx/sites-available/cicd /etc/nginx/sites-enabled/
-#sudo ln -s /etc/nginx/sites-available/cicd /etc/nginx/sites-enabled
+sudo cp /home/ubuntu/test/nginx/nginx.conf /etc/nginx/sites-available/test
+sudo ln -s /etc/nginx/sites-available/test /etc/nginx/sites-enabled/
+#sudo ln -s /etc/nginx/sites-available/test /etc/nginx/sites-enabled
 #sudo nginx -t
 sudo gpasswd -a www-data ubuntu
 sudo systemctl restart nginx
